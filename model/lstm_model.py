b6a5136fbe23787bad5ad9ae73a2f6e5c63abe81
Murilo Juchem <mjuchem@gmail.com>
2020-04-04 16:17:44 -0700

    no message

---
+++ model/lstm_model.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.callbacks import EarlyStopping
+
+import tensorflow_addons as tfa
+
+
+def build_model(vocab_size, num_units, dropout, optimizer):
+
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=50)
+
+    weight_init = RandomNormal(mean=0.0, stddev=0.05, seed=71)
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, num_units), #, input_length=max_length),
+        tf.keras.layers.LSTM(num_units, return_sequences=True, kernel_initializer=weight_init, dropout=dropout),
+        tf.keras.layers.LSTM(num_units, kernel_initializer=weight_init, dropout=dropout),
+        tf.keras.layers.Dense(vocab_size, activation="softmax")
+    ])
+
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['mae', 'acc'])
+
+    return model
---
+++ smiles/smiles_tokenizer.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+
+class SmilesTokenizer(object):
+    def __init__(self):
+        atoms = ['Li', 'Na', 'Al', 'Si', 'Cl', 'Sc', 'Zn', 'As', 'Se',
+                 'Br', 'Sn', 'Te', 'Cn', 'H', 'B', 'C', 'N', 'O',
+                 'F', 'P', 'S', 'K', 'V', 'I', ]
+        special = ['(', ')', '[', ']', '=', '#', '%', '0', '1', '2',
+                   '3', '4', '5', '6', '7', '8', '9', '+', '-', 'se',
+                   'te', 'c', 'n', 'o', 's']
+        padding = ['G', 'A', 'E']
+
+        self.table = sorted(atoms, key=len, reverse=True) + special + padding
+        self.table_len = len(self.table)
+
+        self.table_2_chars = list(filter(lambda x: len(x) == 2, self.table))
+        self.table_1_chars = list(filter(lambda x: len(x) == 1, self.table))
+
+        self.one_hot_dict = {}
+        for i, symbol in enumerate(self.table):
+            vec = np.zeros(self.table_len, dtype=np.float32)
+            vec[i] = 1
+            self.one_hot_dict[symbol] = vec
+
+    def smiles_to_tokens(self, smiles):
+
+        smiles = smiles + ' '
+
+        N = len(smiles)
+
+        token = []
+        i = 0
+
+        while (i < N):
+            c1 = smiles[i]
+            c2 = smiles[i: i + 2]
+
+            if (c2 in self.table_2_chars):
+                token.append(c2)
+                i = i + 1
+                continue
+
+            if (c1 in self.table_1_chars):
+                token.append(c1)
+                i = i + 1
+                continue
+
+            i = i + 1
+
+        return token
+
+    def one_hot_encode(self, tokenized_smiles):
+        result = np.array(
+            [self.one_hot_dict[symbol] for symbol in tokenized_smiles],
+            dtype=np.float32)
+        result = result.reshape(1, result.shape[0], result.shape[1])
+        return result
+
+    def tokens_to_embeddings(self, tokenized_smiles):
+        result = [self.table.index(symbol) for symbol in tokenized_smiles]
+        return result
+
+    def embeddings_to_tokens(self, embeddings):
+        embeddings = embeddings[embeddings != self.zero()]
+        result = [self.table[s] for s in embeddings]
+        return result
+    
+    def embeddings_to_smiles(self, embeddings):
+        tokens = self.embeddings_to_tokens(embeddings)
+        s = ""
+        for t in tokens:
+            s = s + t
+        return s
+
+    def zero(self):
+        return self.table.index('A')
